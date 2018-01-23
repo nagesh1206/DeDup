c06c166a5f3e78359366e7e200de51e53d4c403d
nagesh1206 <nagesh1206@yahoo.co.in>
2018-01-22 23:38:13 -0500

    Add files via upload

--- DeDup/src/main/java/com/sei/DeDup/DeDup.java
+++ DeDup/src/main/java/com/sei/DeDup/DeDup.java
@@ -1,5 +1,6 @@
 package com.sei.DeDup;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -19,7 +20,8 @@ public class DeDup {
 	 * Uses hash set collection which doesn't allow duplicates
 	 * 
 	 * This is best suited for faster performance with no order and sorting
-	 *  
+	 * 
+	 * @param randomIntegers
 	 * @return int[]
 	 */
 	public int[] uniqueNumbers(int[] randomIntegers) {
@@ -30,12 +32,68 @@ public class DeDup {
 		return numbersSet.stream().mapToInt(Integer::intValue).toArray();
 	}
 	
+	/**
+	 * Since the ask is for new array without duplicates using Arrays.copy.
+	 * If not, same array with index can be used  without creating new array.
+	 * 
+	 * @param randomIntegers
+	 * @return int[]
+	 */
+	public int[] uniqueNumbersAfterSorting(int[] randomIntegers) {
+		int[] uniqueNumbers = null;
+		if (randomIntegers != null && randomIntegers.length > 0) {
+			int arrLength = randomIntegers.length;
+			if (arrLength == 0 || arrLength == 1) {
+				uniqueNumbers = randomIntegers;
+			} else {
+				Arrays.sort(randomIntegers);
+				int j = 0;
+				for (int i = 0; i < arrLength - 1; i++) {
+					if (randomIntegers[i] != randomIntegers[i + 1]) {
+						randomIntegers[j++] = randomIntegers[i];
+					}
+				}
+				randomIntegers[j++] = randomIntegers[arrLength - 1];
+				uniqueNumbers = Arrays.copyOf(randomIntegers, j);
+			}
+		}
+		return uniqueNumbers;
+	}
+	
+	
+	/**
+	 * This solution is without any use of collections
+	 * 
+	 *  @param randomIntegers
+	 * @return int[]
+	 */
+	public int[] uniqueNumbersWithoutCollection(int[] randomIntegers) {
+
+		int[] uniqueNumbers = null;
+		if(randomIntegers != null && randomIntegers.length > 0) {
+			
+			int totalUniqueNumbers = randomIntegers.length;
+
+			for (int i = 0; i < totalUniqueNumbers; i++) {
+				for (int j = i + 1; j < totalUniqueNumbers; j++) {
+					if (randomIntegers[i] == randomIntegers[j]) {
+						randomIntegers[j] = randomIntegers[totalUniqueNumbers - 1];
+						totalUniqueNumbers--;
+						j--;
+					}
+				}
+			}
+			uniqueNumbers = Arrays.copyOf(randomIntegers, totalUniqueNumbers);
+		}
+		return uniqueNumbers;
+	}
 	
 	/**
 	 * Uses linked hash set collection which doesn't allow duplicates and maintains order of insertion
 	 * 
 	 * This is slower in performance than HashSet but its used when the insertion order needs to be maintained 
 	 * 
+	 * @param randomIntegers
 	 * @return int[]
 	 */
 	public int[] uniqueNumbersWithInsertionOrder(int[] randomIntegers) {
@@ -51,6 +109,7 @@ public class DeDup {
 	 * 
 	 * This is slower in performance than HashSet but its used when the data needs to be sorted
 	 * 
+	 * @param randomIntegers
 	 * @return int[]
 	 */
 	public int[] uniqueNumbersWithSortOrder(int[] randomIntegers) {
